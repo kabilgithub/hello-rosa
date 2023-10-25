581fa7ed73a6880f20a869c7287c85d5635c8151
JK <kabiljayaraman@gmail.com>
2023-10-25 11:19:43 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.get('/api/rosa', (req, res) => {
+  res.send('Hello ROSA!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
